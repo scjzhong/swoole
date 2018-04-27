576328333395d99f77fd2a3fdf0a8ce68af2e122
scjzhong <scjzhong@sina.com>
2018-04-27 18:12:37 +0800

    1

--- asyncMysql.php
+++ asyncMysql.php
@@ -1,16 +1,31 @@
 <?php
 
 $db = new Swoole\MySQL;
-$server = array(
-    'host' => '127.0.0.1',
-    'user' => 'test',
-    'password' => 'test',
-    'database' => 'test',
-);
+$server = [
+    'host'     => '47.100.161.112',
+    'user'     => 'root',
+    'password' => 'nihao123###',
+    'database' => 'swoole',
+    'charset'  => 'utf-8',
+    'timeout'  => 2,  // 可选：连接超时时间（非查询超时时间），默认为SW_MYSQL_CONNECT_TIMEOUT（1.0）
+];
 
-$db->connect($server, function ($db, $result) {
-    $db->query("show tables", function (Swoole\MySQL $db, $result) {
-        var_dump($result);
+$db->connect($server, function ($db, $r) {
+    if ($r === false) {
+        var_dump($db->connect_errno, $db->connect_error);
+        die;
+    }
+    $sql = 'show tables';
+    $db->query($sql, function(swoole_mysql $db, $r) {
+        if ($r === false)
+        {
+            var_dump($db->error, $db->errno);
+        }
+        elseif ($r === true )
+        {
+            var_dump($db->affected_rows, $db->insert_id);
+        }
+        var_dump($r);
         $db->close();
     });
 });
